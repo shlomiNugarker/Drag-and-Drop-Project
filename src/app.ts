6584275d944eb41342760976dcadc7842c0cdf10
Shlomi Nugarker <shlomin1231@gmail.com>
2022-09-29 21:28:29 +0300

    added render func

--- src/app.ts
+++ src/app.ts
@@ -62,6 +62,38 @@ function autoBind(
   return adjDescirptor
 }
 
+// ProjectList Class
+class ProjectList {
+  templateElement: HTMLTemplateElement
+  hostElement: HTMLDivElement
+  element: HTMLElement
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById(
+      'project-list'
+    )! as HTMLTemplateElement
+
+    this.hostElement = <HTMLDivElement>document.getElementById('app')!
+
+    const importedNode = document.importNode(this.templateElement.content, true)
+    this.element = importedNode.firstElementChild as HTMLElement
+    this.element.id = `${this.type}-projects`
+    this.attach()
+    this.renderContant()
+  }
+
+  private renderContant() {
+    const listId = `${this.type}-project-list`
+    this.element.querySelector('ul')!.id = listId
+    this.element.querySelector('h2')!.textContent =
+      this.type.toUpperCase() + ' PROJECTS'
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element)
+  }
+}
+
 // ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement
@@ -159,3 +191,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput()
+const activePrjList = new ProjectList('active')
+const finishedPrjList = new ProjectList('finished')
