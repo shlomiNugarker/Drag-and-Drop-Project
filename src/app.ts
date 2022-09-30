ed53d3da39bcdc8a8ce754e827cc0bde6873c304
Shlomi Nugarker <shlomin1231@gmail.com>
2022-09-30 17:19:21 +0300

    render Project Item with class, addad getter

--- src/app.ts
+++ src/app.ts
@@ -159,6 +159,33 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void
 }
 
+// ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLinkElement> {
+  // <- generics: first= hostElement, second = element
+  private project: Project
+
+  get persons() {
+    if (this.project.people === 1) return '1 person'
+    return `${this.project.people} persons`
+  }
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id)
+    this.project = project
+
+    this.configure()
+    this.renderContent()
+  }
+
+  configure() {}
+
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned'
+    this.element.querySelector('p')!.textContent = this.project.description
+  }
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[]
@@ -170,30 +197,36 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
 
     this.configure()
     this.renderContent()
-
-    this.renderContent()
   }
 
-  private renderProjects() {
-    const listEl = document.getElementById(
-      `${this.type}-projects-list`
-    )! as HTMLUListElement
-
-    listEl.innerHTML = ''
-    for (const prjItem of this.assignedProjects) {
-      const listItem = document.createElement('li')
-      listItem.textContent = prjItem.title
-      listEl.appendChild(listItem)
-    }
+  configure() {
+    projectState.addListener((projects: Project[]) => {
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === 'active') {
+          return prj.status === ProjectStatus.Active
+        }
+        return prj.status === ProjectStatus.Finished
+      })
+      this.assignedProjects = relevantProjects
+      this.renderProjects()
+    })
   }
-
-  configure() {}
 
   renderContent() {
     const listId = `${this.type}-projects-list`
     this.element.querySelector('ul')!.id = listId
     this.element.querySelector('h2')!.textContent =
       this.type.toUpperCase() + ' PROJECTS'
+  }
+
+  private renderProjects() {
+    const listEl = document.getElementById(
+      `${this.type}-projects-list`
+    )! as HTMLUListElement
+    listEl.innerHTML = ''
+    for (const prjItem of this.assignedProjects) {
+      new ProjectItem(this.element.querySelector('ul')!.id, prjItem)
+    }
   }
 }
 
@@ -250,14 +283,11 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
       !validate(titleValidatable) ||
       !validate(descriptionValidatable) ||
       !validate(peopleValidatable)
-      // enterdTitle.trim().length === 0 ||
-      // enterdDescription.trim().length === 0 ||
-      // enteredPeople.trim().length === 0
     ) {
       alert('Invalid input, please try again')
       return
     } else {
-      return [enterdTitle, enterdDescription, +enteredPeople] // return Tuple
+      return [enterdTitle, enterdDescription, +enteredPeople] // returns Tuple
     }
   }
 
