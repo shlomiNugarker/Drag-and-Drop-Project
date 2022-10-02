fa91d33ede57a3c84bbd56fd5013bd61f1d98f91
Shlomi Nugarker <shlomin1231@gmail.com>
2022-10-02 11:23:47 +0300

    Organized files & folders

---
+++ src/components/project-item.ts
@@ -0,0 +1,47 @@
+/// <reference path="base-component.ts" />
+
+namespace App {
+  // ProjectItem Class
+  export class ProjectItem
+    extends Component<HTMLUListElement, HTMLLinkElement>
+    implements Draggable
+  {
+    // <- generics: first= hostElement, second = element
+    private project: Project
+
+    get persons() {
+      if (this.project.people === 1) return '1 person'
+      return `${this.project.people} persons`
+    }
+
+    constructor(hostId: string, project: Project) {
+      super('single-project', hostId, false, project.id)
+      this.project = project
+
+      this.configure()
+      this.renderContent()
+    }
+
+    @autoBind
+    dragStartHandler(event: DragEvent): void {
+      console.log(event)
+      event.dataTransfer!.setData('text/plain', this.project.id)
+      event.dataTransfer!.effectAllowed = 'move'
+    }
+
+    dragEndHandler(_: DragEvent): void {
+      console.log('dragEndHandler')
+    }
+
+    configure() {
+      this.element.addEventListener('dragstart', this.dragStartHandler)
+      this.element.addEventListener('dragend', this.dragEndHandler)
+    }
+
+    renderContent() {
+      this.element.querySelector('h2')!.textContent = this.project.title
+      this.element.querySelector('h3')!.textContent = this.persons + ' assigned'
+      this.element.querySelector('p')!.textContent = this.project.description
+    }
+  }
+}
