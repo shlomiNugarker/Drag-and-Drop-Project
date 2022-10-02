ee276181ba9dcbdaf1464bde0fa063c5722148e1
Shlomi Nugarker <shlomin1231@gmail.com>
2022-10-02 12:14:35 +0300

    moved to ES6 modules

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,82 +1,83 @@
-/// <reference path="base-component.ts" />
+import { Component } from './base-component.js'
+import { Validatable, validate } from '../util/validation.js'
+import { autoBind } from '../decorators/autobind.js'
+import { projectState } from '../state/project-state.js'
 
-namespace App {
-  // ProjectInput Class
-  export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
-    titleInputElement: HTMLInputElement
-    descriptionInputElement: HTMLInputElement
-    peopleInputElement: HTMLInputElement
+// ProjectInput Class
+export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+  titleInputElement: HTMLInputElement
+  descriptionInputElement: HTMLInputElement
+  peopleInputElement: HTMLInputElement
 
-    constructor() {
-      super('project-input', 'app', true, 'user-input')
+  constructor() {
+    super('project-input', 'app', true, 'user-input')
 
-      this.titleInputElement = this.element.querySelector(
-        '#title'
-      ) as HTMLInputElement
-      this.descriptionInputElement = this.element.querySelector(
-        '#description'
-      ) as HTMLInputElement
-      this.peopleInputElement = this.element.querySelector(
-        '#people'
-      ) as HTMLInputElement
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement
 
-      this.configure()
+    this.configure()
+  }
+
+  configure() {
+    this.element.addEventListener('submit', this.submitHandler)
+  }
+
+  renderContent() {}
+
+  private getherUserInput(): [string, string, number] | void {
+    const enterdTitle = this.titleInputElement.value
+    const enterdDescription = this.descriptionInputElement.value
+    const enteredPeople = this.peopleInputElement.value
+
+    const titleValidatable: Validatable = {
+      value: enterdTitle,
+      required: true,
+    }
+    const descriptionValidatable: Validatable = {
+      value: enterdDescription,
+      required: true,
+      minLength: 5,
+    }
+    const peopleValidatable: Validatable = {
+      value: enteredPeople,
+      required: true,
+      min: 1,
+      max: 5,
     }
 
-    configure() {
-      this.element.addEventListener('submit', this.submitHandler)
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert('Invalid input, please try again')
+      return
+    } else {
+      return [enterdTitle, enterdDescription, +enteredPeople] // returns Tuple
     }
+  }
 
-    renderContent() {}
+  private clearInputs() {
+    this.titleInputElement.value = ''
+    this.descriptionInputElement.value = ''
+    this.peopleInputElement.value = ''
+  }
 
-    private getherUserInput(): [string, string, number] | void {
-      const enterdTitle = this.titleInputElement.value
-      const enterdDescription = this.descriptionInputElement.value
-      const enteredPeople = this.peopleInputElement.value
-
-      const titleValidatable: Validatable = {
-        value: enterdTitle,
-        required: true,
-      }
-      const descriptionValidatable: Validatable = {
-        value: enterdDescription,
-        required: true,
-        minLength: 5,
-      }
-      const peopleValidatable: Validatable = {
-        value: enteredPeople,
-        required: true,
-        min: 1,
-        max: 5,
-      }
-
-      if (
-        !validate(titleValidatable) ||
-        !validate(descriptionValidatable) ||
-        !validate(peopleValidatable)
-      ) {
-        alert('Invalid input, please try again')
-        return
-      } else {
-        return [enterdTitle, enterdDescription, +enteredPeople] // returns Tuple
-      }
-    }
-
-    private clearInputs() {
-      this.titleInputElement.value = ''
-      this.descriptionInputElement.value = ''
-      this.peopleInputElement.value = ''
-    }
-
-    @autoBind
-    private submitHandler(event: Event) {
-      event.preventDefault()
-      const userInput = this.getherUserInput()
-      if (Array.isArray(userInput)) {
-        const [title, desc, people] = userInput
-        projectState?.addProject(title, desc, people)
-        this.clearInputs()
-      }
+  @autoBind
+  private submitHandler(event: Event) {
+    event.preventDefault()
+    const userInput = this.getherUserInput()
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput
+      projectState?.addProject(title, desc, people)
+      this.clearInputs()
     }
   }
 }
